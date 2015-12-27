17a861f774e5619b90b6b018f1a154b83b2e1f9d
Amal Bose <axatrikx@gmail.com>
2015-12-27 07:14:21 +0530

    added __str__ method

--- projectadmin/models.py
+++ projectadmin/models.py
@@ -26,3 +26,6 @@ class UserProfile(models.Model):
     # Override the __unicode__() method to return out something meaningful!
     def __unicode__(self):
         return self.user.username
+    
+    def __str__(self):
+        return self.user.username
